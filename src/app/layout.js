402a2be78d4610597a1ba614e54e83a195b0e2e2
alitayin <alitaweb3@gmail.com>
2024-06-13 10:56:46 +0800

    Preview card4

--- src/app/layout.js
+++ src/app/layout.js
@@ -20,7 +20,7 @@ export default function RootLayout({ children }) {
       <head>
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content="eCash Chat" />
-        <meta name="twitter:image" content="https://im.gurl.eu.org/file/bc5fee52273da041b583a.png" />
+        <meta name="twitter:image" content="https://im.gurl.eu.org/file/a53a1ce61457c72ec55c7.jpg" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
