afb2b01a5c2bc775079354a93983dad671c7c136
alitayin <alitaweb3@gmail.com>
2024-06-13 10:49:58 +0800

    Preview card3

--- src/app/page.js
+++ src/app/page.js
@@ -486,21 +486,17 @@ export default function Home() {
                 </Modal.Body>
                 <Modal.Footer>
                     <div className="mt-5 sm:mt-6 sm:grid sm:grid-flow-row-dense sm:grid-cols-2 sm:gap-3">
-                          <button
-                            type="button"
-                            className="inline-flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 sm:col-start-2"
+                          <Button   
                             onClick={() => saveLoginAddressToLocalStorage()}
                           >
                             Save login
-                          </button>
-                          <button
-                            type="button"
-                            className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:col-start-1 sm:mt-0"
+                          </Button>
+                          <Button
                             onClick={() => setOpenSaveLoginModal(false)}
                             data-autofocus
                           >
                             Don't save login
-                          </button>
+                          </Button>
                         </div>
                 </Modal.Footer>
             </Modal>
