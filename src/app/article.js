10cba9d9389da54343da905fb875a795cb14aede
alitayin <alitaweb3@gmail.com>
2024-08-03 08:20:08 +0800

    improve add new contact layout 2

--- src/app/article.js
+++ src/app/article.js
@@ -453,10 +453,13 @@ export default function Article( { chronik, address, isMobile, sharedArticleTxid
                                             <IdCardIcon className="h-4 w-4" />
                                         </Button>
                                     </PopoverTrigger>
-                                    <PopoverContent className="w-120">
+                                    <PopoverContent>
                                         <div className="space-y-2">
-                                            <h4 className="font-medium leading-none">New contact</h4>
-                                            <p className="text-sm text-muted-foreground">
+                                        <h4 className="flex items-center font-medium leading-none">
+                                                <Pencil1Icon className="h-4 w-4 mr-1" />
+                                                New contact
+                                            </h4>
+                                            <p className="text-sm text-muted-foreground max-w-96 break-words">
                                                 Input contact name for <br />{foundReply.replyAddress}
                                             </p>
                                         </div>
@@ -902,7 +905,10 @@ export default function Article( { chronik, address, isMobile, sharedArticleTxid
                                         </PopoverTrigger>
                                         <PopoverContent>
                                             <div className="space-y-2">
-                                                <h4 className="font-medium leading-none">New contact</h4>
+                                            <h4 className="flex items-center font-medium leading-none">
+                                                <Pencil1Icon className="h-4 w-4 mr-1" />
+                                                New contact
+                                            </h4>
                                                 <p className="text-sm text-muted-foreground max-w-96 break-words">
                                                     Input contact name for <br />{tx.replyAddress}
                                                 </p>
