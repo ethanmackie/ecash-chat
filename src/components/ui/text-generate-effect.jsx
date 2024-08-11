77dc41cd076446242280ae97730106d3ae25fda8
alitayin <alitaweb3@gmail.com>
2024-08-11 17:54:39 +0800

    a login area for mobile and pc and import some new animation component and some other improvement

---
+++ src/components/ui/text-generate-effect.jsx
@@ -0,0 +1,51 @@
+"use client";
+import { useEffect } from "react";
+import { motion, stagger, useAnimate } from "framer-motion";
+import { cn } from "@/lib/utils";
+
+export const TextGenerateEffect = ({
+  words,
+  className,
+  filter = true,
+  duration = 0.2
+}) => {
+  const [scope, animate] = useAnimate();
+  let wordsArray = words.split(" ");
+  
+  useEffect(() => {
+    animate("span", {
+      opacity: 1,
+      filter: filter ? "blur(0px)" : "none",
+    }, {
+      duration: duration ? duration : 1,
+      delay: stagger(0.2),
+    });
+  }, [scope.current]);
+
+  const renderWords = () => {
+    return (
+      <motion.div ref={scope}>
+        {wordsArray.map((word, idx) => (
+          <motion.span
+            key={word + idx}
+            className="opacity-0"
+            style={{
+              filter: filter ? "blur(10px)" : "none",
+            }}>
+            {word}{" "}
+          </motion.span>
+        ))}
+      </motion.div>
+    );
+  };
+
+  return (
+    <div className={cn(className)}>
+      <div className="mt-4">
+        <div>
+          {renderWords()}
+        </div>
+      </div>
+    </div>
+  );
+};
