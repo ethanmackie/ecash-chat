09f55c6951dcad5d6b959767d2990c0566b97298
alitayin <alitaweb3@gmail.com>
2024-10-15 14:21:39 +0800

    sync

--- src/app/layout.js
+++ src/app/layout.js
@@ -45,7 +45,7 @@ export default function RootLayout({ children }) {
         )}>
            <ThemeProvider
             attribute="class"
-            defaultTheme="system"
+            defaultTheme="light"
             enableSystem
             disableTransitionOnChange
           >
