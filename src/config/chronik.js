6f7f077da7c94aeb005085ec93d7200f5c9d7593
ethanmackie <ethanmackie1199@gmail.com>
2025-01-10 13:16:12 +1100

    Switch back to prod chronik

--- src/config/chronik.js
+++ src/config/chronik.js
@@ -1,10 +1,12 @@
 export const chronik = {
     urls: [
-        'https://chronik-native.fabien.cash',
+        
         'https://chronik-native1.fabien.cash',
         'https://chronik-native2.fabien.cash',
+        'https://chronik-native3.fabien.cash',
         'https://chronik.pay2stay.com/xec',
         'https://chronik.be.cash/xec2',
+        'https://chronik-native.fabien.cash',
     ],
     txHistoryPageSize: 200,
     articleHistoryPageSize: 25,
