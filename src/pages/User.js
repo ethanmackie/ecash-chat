2af435b0b5fb5becfef206602221cbc14f876eb4
ethanmackie <ethanmackie1199@gmail.com>
2025-01-19 01:01:01 +1100

    User content page scaffolding

---
+++ src/pages/User.js
@@ -0,0 +1,40 @@
+import { useEffect, useState } from 'react';
+import { useSearchParams } from 'next/navigation';
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+  } from "@/components/ui/card"
+import { AnonAvatar } from "@/components/ui/social";
+
+export default function User() {
+    const searchParams = useSearchParams();
+    const address = searchParams.get("address");
+    return (
+        <>
+            <Card className="mt-2">
+                <CardHeader>
+                    <div className="flex items-center">
+                    <div>
+                        <AnonAvatar />
+                    </div>
+                    <div className="ml-3">
+                        <p className="font-semibold leading-none tracking-tight">Address</p>
+                        <p className="text-sm text-muted-foreground max-w-lg break-words text-balance leading-relaxed">
+                        {address ? `${address.substring(0, 11)}...${address.substring(address.length - 5)}` : ''}
+                        </p>
+                    </div>
+                    </div>
+                </CardHeader>
+                <CardContent>
+                    <CardTitle>Balance</CardTitle>
+                    <CardDescription className="max-w-lg break-words text-balance leading-relaxed">
+                    100,000,000 XEC
+                    </CardDescription>
+                </CardContent>
+            </Card>
+        </>
+    )
+};
